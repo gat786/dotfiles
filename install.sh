dcdd684a9815218b4d24cef6c8d2957fe4007840
Ganesh Tiwari <ganesht049@gmail.com>
2019-12-28 20:18:45 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,1 @@
-wget https://julialang-s3.julialang.org/bin/linux/x64/1.2/julia-1.2.0-linux-x86_64.tar.gz
-tar -xzf julia-1.2.0-linux-x86_64.tar.gz
-cat "export PATH=$PATH:/home/vsonline/julia-1.2.0/bin" > $HOME/.bashrc
+apt install julia
