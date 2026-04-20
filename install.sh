f4feee30f27cf955ee56e62d2b856b707fd84cf3
Ganesh Tiwari <ganesht049@gmail.com>
2026-04-20 17:59:24 +0530

    feat: added brew installer and bundle file

--- install.sh
+++ install.sh
@@ -1,1 +1,18 @@
-sudo apt install julia -y
+#!/bin/zsh
+
+OUTPUT_DIR=~/.dotfiles-downloads
+if [[ -d $OUTPUT_DIR ]]; then
+    echo "$OUTPUT_DIR directory exists already, cleaning it and recreating";
+    rm -rf $OUTPUT_DIR && mkdir -p $OUTPUT_DIR;
+else
+    echo "Creating $OUTPUT_DIR";
+    mkdir -p $OUTPUT_DIR;
+fi;
+
+BREW_VERSION=5.1.7;
+wget https://github.com/Homebrew/brew/releases/download/$BREW_VERSION/Homebrew.pkg \
+    -O $OUTPUT_DIR/Homebrew.pkg
+
+sudo installer -verbose -pkg $OUTPUT_DIR/Homebrew.pkg -target ~
+
+brew bundle install
