9665d8528f7a7c8fdd6901c95852a3da7316c17d
Ganesh Tiwari <ganesht049@gmail.com>
2026-04-20 18:41:52 +0530

    feat: brew installation check added

--- install.sh
+++ install.sh
@@ -9,10 +9,20 @@ else
     mkdir -p $OUTPUT_DIR;
 fi;
 
-BREW_VERSION=5.1.7;
-curl -L https://github.com/Homebrew/brew/releases/download/$BREW_VERSION/Homebrew.pkg \
-    --output $OUTPUT_DIR/Homebrew.pkg
+# this check is zsh specific, won't work anywhere else,
+# it checks whether brew is installed or not, if it is present
+# it skips installation
+if ! (( $+commands[brew] )); then
+    echo "Brew is not installed installing it.";
+    BREW_VERSION=5.1.7;
+    curl -L https://github.com/Homebrew/brew/releases/download/$BREW_VERSION/Homebrew.pkg \
+        --output $OUTPUT_DIR/Homebrew.pkg;
 
-sudo installer -verbose -pkg $OUTPUT_DIR/Homebrew.pkg -target ~
+    sudo installer -verbose -pkg $OUTPUT_DIR/Homebrew.pkg -target ~
+    echo "Brew installation done.";
+else
+    echo "Brew already present, no need to install.";
+fi
 
-brew bundle install
+
+brew bundle check
